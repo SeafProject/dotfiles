23ff6039566cc3edbf4de10488fb9b2de531c7dd
Hajime MATSUMOTO <mail@hazime.org>
2014-10-01 15:59:07 +0000

    Vimソース読みやすくした

--- bin/setup.d/vim.sh
+++ bin/setup.d/vim.sh
@@ -6,53 +6,20 @@ NEOBUNDLE_REPOS=https://github.com/Shougo/neobundle.vim
 VIM_REPOS="https://vim.googlecode.com/hg/ vim"
 
 [ -d "$HOME/.vim" ] || mkdir -v $HOME/.vim
+[ -d "$HOME/.vim/tmp" ] || mkdir -v $HOME/.vim/tmp
 [ -d "$HOME/.vim/bundle" ] || mkdir -v $HOME/.vim/bundle
-
 [ -d "$HOME/.vim/bundle/neobundle.vim" ] || git clone $NEOBUNDLE_REPOS $HOME/.vim/bundle/neobundle.vim
 
-[ -f "$HOME/.vimrc" ] || ln -s "$DIR/_vimrc" "$HOME/.vimrc"
+rm -rf $HOME/.vimrc*
 
-ctags --version > /dev/null || $DIR/setup.d/ctags.sh
-
-vim --version | grep +lua > /dev/null
-
-if [ $? -ne 0 ]
-then
-	sudo apt-get install \
-		mercurial \
-		lua5.2 liblua5.2 liblua5.2-dev \
-		liblua5.1-dev \
-		libperl-dev \
-		python3 \
-		libpython3-dev \
-		libtool-doc autoconf automake gcj-jdk \
-		autotools-dev libltdl-dev libltdl7 \
-		libreadline-dev libreadline6-dev libtinfo-dev libtool pkg-config
-
-
-	if [ -f /usr/local/bin/vim ]
-	then
-		pushd /usr/local/src
-			sudo hg clone $VIM_REPOS
-			pushd vim/src
-				sudo ./configure \
-					--with-features=huge \
-					--enable-multibyte \
-					--prefix=/usr/local \
-					--enable-gui=gnome2 \
-					--enable-luainterp=yes \
-					--with-compiledby="Hajime MATSUMOTO <mail@hazime.org>" \
-					--enable-fail-if-missing
-
-				[ $? -ne 0 ] && exit $?
-				sudo make
-				sudo make install
-			popd
-		popd
-
-		sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim 100
-	fi
-fi
+ln -s "$DIR/vimrc.d/_vimrc" "$HOME/.vimrc"
+ln -s "$DIR/vimrc.d/_vimrc.basic" "$HOME/.vimrc.basic"
+ln -s "$DIR/vimrc.d/_vimrc.keybind" "$HOME/.vimrc.keybind"
+ln -s "$DIR/vimrc.d/_vimrc.neoBundle" "$HOME/.vimrc.neoBundle"
+ln -s "$DIR/vimrc.d/_vimrc.neoComplete" "$HOME/.vimrc.neoComplete"
+ln -s "$DIR/vimrc.d/_vimrc.dwm" "$HOME/.vimrc.dwm"
+ln -s "$DIR/vimrc.d/_vimrc.unite" "$HOME/.vimrc.unite"
+ln -s "$DIR/vimrc.d/_vimrc.look_and_feel" "$HOME/.vimrc.look_and_feel"
 
 
 /usr/local/bin/vim +NeoBundleInstall +q
---
+++ install.d/compile_vim.sh
@@ -0,0 +1,36 @@
+VIM_REPOS="https://vim.googlecode.com/hg/ vim"
+
+sudo apt-get install \
+	ctags \
+	mercurial \
+	lua5.2 liblua5.2 liblua5.2-dev \
+	liblua5.1-dev \
+	libperl-dev \
+	python3 \
+	libpython3-dev \
+	libtool-doc autoconf automake gcj-jdk \
+	autotools-dev libltdl-dev libltdl7 \
+	libreadline-dev libreadline6-dev libtinfo-dev libtool pkg-config
+
+
+pushd /usr/local/src
+	sudo hg clone $VIM_REPOS
+	pushd vim/src
+		sudo ./configure \
+			--with-features=huge \
+			--enable-multibyte \
+			--prefix=/usr/local \
+			--enable-gui=gnome2 \
+			--enable-luainterp=yes \
+			--with-compiledby="Hajime MATSUMOTO <mail@hazime.org>" \
+			--enable-fail-if-missing
+
+		[ $? -ne 0 ] && exit $?
+		sudo make
+		sudo make install
+	popd
+popd
+
+echo "Vim To Add Alternatives As Default Editor"
+echo "update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim 100"
+
