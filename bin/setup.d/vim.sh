65906d001f9cbf13ac3cda008777faf31d9c1516
Hajime MATSUMOTO <mail@hazime.org>
2014-10-02 15:45:38 +0000

    VIM PowerLine対応

--- bin/setup.d/vim.sh
+++ bin/setup.d/vim.sh
@@ -20,11 +20,42 @@ ln -s "$DIR/vimrc.d/_vimrc.neoComplete" "$HOME/.vimrc.neoComplete"
 ln -s "$DIR/vimrc.d/_vimrc.dwm" "$HOME/.vimrc.dwm"
 ln -s "$DIR/vimrc.d/_vimrc.unite" "$HOME/.vimrc.unite"
 ln -s "$DIR/vimrc.d/_vimrc.look_and_feel" "$HOME/.vimrc.look_and_feel"
+ln -s "$DIR/vimrc.d/_vimrc.powerline" "$HOME/.vimrc.powerline"
 
 if [ ! -e $HOME/.vimrc.local ]
 then
 	touch $HOME/.vimrc.local
 fi
 
+# Python Package Installer
+sudo apt-get install python-pip
+
+pip -V > /dev/null
+if [ $? -ne 0 ]
+then
+	sudo su -c 'pip list' | grep powerline-status > /dev/null
+	sudo pip install -U pip
+fi
+
+# Install Powerline
+powerline -h > /dev/null
+if [ $? -ne 0 ]
+then
+	sudo su -c 'pip install --user git+git://github.com/Lokaltog/powerline'
+fi
+
+PowerlineSymbols="https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf"
+PowerlineSymbolsConf="https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf"
+
+if [ ! -e /usr/share/doc/PowerlineSymbols.otf ]
+then
+	sudo wget $PowerlineSymbols -O /usr/share/doc/PowerlineSymbols.otf
+	sudo fc-cache -vf
+fi
+
+if [ ! -e /etc/fonts/conf.d/10-powerline-symbols.conf ]
+then
+	sudo wget $PowerlineSymbolsConf -O /etc/fonts/conf.d/10-powerline-symbols.conf
+fi
 
 /usr/local/bin/vim +NeoBundleInstall +q
