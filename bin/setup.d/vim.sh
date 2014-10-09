6db5ff593952fc70ceb8563901bba62b13eba938
Hajime MATSUMOTO <mail@hazime.org>
2014-10-10 02:08:56 +0900

    Update Vim

--- bin/setup.d/vim.sh
+++ bin/setup.d/vim.sh
@@ -28,34 +28,34 @@ then
 fi
 
 # Python Package Installer
-sudo apt-get install python-pip
+#sudo apt-get install python-pip
 
-pip -V > /dev/null
-if [ $? -ne 0 ]
-then
-	sudo su -c 'pip list' | grep powerline-status > /dev/null
-	sudo pip install -U pip
-fi
-
-# Install Powerline
-powerline -h > /dev/null
-if [ $? -ne 0 ]
-then
-	sudo su -c 'pip install --user git+git://github.com/Lokaltog/powerline'
-fi
-
-PowerlineSymbols="https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf"
-PowerlineSymbolsConf="https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf"
-
-if [ ! -e /usr/share/doc/PowerlineSymbols.otf ]
-then
-	sudo wget $PowerlineSymbols -O /usr/share/doc/PowerlineSymbols.otf
-	sudo fc-cache -vf
-fi
-
-if [ ! -e /etc/fonts/conf.d/10-powerline-symbols.conf ]
-then
-	sudo wget $PowerlineSymbolsConf -O /etc/fonts/conf.d/10-powerline-symbols.conf
-fi
-
+#pip -V > /dev/null
+#if [ $? -ne 0 ]
+#then
+#	sudo su -c 'pip list' | grep powerline-status > /dev/null
+#	sudo pip install -U pip
+#fi
+#
+## Install Powerline
+#powerline -h > /dev/null
+#if [ $? -ne 0 ]
+#then
+#	sudo su -c 'pip install --user git+git://github.com/Lokaltog/powerline'
+#fi
+#
+#PowerlineSymbols="https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf"
+#PowerlineSymbolsConf="https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf"
+#
+#if [ ! -e /usr/share/doc/PowerlineSymbols.otf ]
+#then
+#	sudo wget $PowerlineSymbols -O /usr/share/doc/PowerlineSymbols.otf
+#	sudo fc-cache -vf
+#fi
+#
+#if [ ! -e /etc/fonts/conf.d/10-powerline-symbols.conf ]
+#then
+#	sudo wget $PowerlineSymbolsConf -O /etc/fonts/conf.d/10-powerline-symbols.conf
+#fi
+#
 /usr/local/bin/vim +NeoBundleInstall +q
